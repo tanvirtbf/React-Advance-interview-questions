ec09e3e9903573fbfd6bd1a9aab5b2d68cd4c356
tanvirtbf <tanver018765@gmail.com>
2024-05-11 12:23:06 +0600

    understand react jsx deeply

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css";
 
 function App() {
+  const a = 2;
+  const b = 3;
+  function add(a,b){
+    return a+b;
+  }
   return (
     <>
       <h1>Hello World!</h1>
+      <h1>{a}</h1>
+      <h1>{b}</h1>
+      <h1>{add(a,b)}</h1>
+      <h1>{new Date().toLocaleDateString()}</h1>
     </>
   );
 }
