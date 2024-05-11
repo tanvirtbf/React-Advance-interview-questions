2b76a436ed2391584c014879af475f0afe48cdbb
tanvirtbf <tanver018765@gmail.com>
2024-05-11 12:35:39 +0600

    shortcut of list of items render

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -8,11 +8,13 @@ function App() {
     'humaira',
     'ayesha'
   ]
+  const itemsList = items.map((item) => <li key={item}>{item}</li>)
   return (
     <>
       <h1 style={{backgroundColor: 'red',fontSize:'25px'}}>Hello World!</h1>
       {items}
-      {items.map(item => <li key={item}>{item}</li>)}
+      {/* {items.map(item => <li key={item}>{item}</li>)} */}
+      {itemsList}
     </>
   );
 }
