ec320a7a022981646b2be74c442d198f24a990ab
tanvirtbf <tanver018765@gmail.com>
2024-05-11 12:26:47 +0600

    conditional rendering using turnary operator

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,18 +1,11 @@
 import "./App.css";
 
 function App() {
-  const a = 2;
-  const b = 3;
-  function add(a,b){
-    return a+b;
-  }
+  const userName = ''
   return (
     <>
       <h1>Hello World!</h1>
-      <h1>{a}</h1>
-      <h1>{b}</h1>
-      <h1>{add(a,b)}</h1>
-      <h1>{new Date().toLocaleDateString()}</h1>
+      {userName? <h1>{userName}</h1> : null}
     </>
   );
 }
