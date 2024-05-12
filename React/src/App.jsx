1b165be32caa40e76aaeb222426054965c80ff03
tanvirtbf <tanver018765@gmail.com>
2024-05-12 14:39:34 +0600

    create simple project named mouseover axis image resize

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -2,21 +2,19 @@ import { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [num, setNum] = useState(0)
-  const handleClick = () => {
-    // setNum(num + 1)
-    // setNum(num + 1)
-    // setNum(num + 1)
-    // setNum(num + 1)
-    setNum((prevState) => prevState + 1)
-    setNum((prevState) => prevState + 1)
-    setNum((prevState) => prevState + 1)
-    setNum((prevState) => prevState + 1)
-  }
+  const [mousePos, setMousePos] = useState({x:0, y:0})
+  const [width,setWidth] = useState(100)
+  const [height,setHeight] = useState(100)
+
+  document.addEventListener("mousemove", (data) => {
+    setMousePos({x: data.clientX, y:data.clientY})
+    setWidth(mousePos.x)
+    setHeight(mousePos.y)
+  })
   return (
     <>
-      <h1>Number : {num} </h1>
-      <button onClick={handleClick}>Click</button>
+      <div> X - {mousePos.x}, Y - {mousePos.y} </div>
+      <img src="https://cdn.beautybooth.com.bd/uploads/all/2FK8ylB5lJrDP9PnIGzensEaeaDcvT7jAD0ZGLxH.jpg" alt="" style={{width:`${width}px`,height:`${height}px`}} />
     </>
   );
 };
