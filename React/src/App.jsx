ef347a6da007ca3fc4fdeb0a2ec41dec79a584ac
tanvirtbf <tanver018765@gmail.com>
2024-05-12 12:12:35 +0600

    create prevState

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -2,31 +2,21 @@ import { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  let [formObj,setFormObj] = useState({})
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formObj)
+  const [num, setNum] = useState(0)
+  const handleClick = () => {
+    // setNum(num + 1)
+    // setNum(num + 1)
+    // setNum(num + 1)
+    // setNum(num + 1)
+    setNum((prevState) => prevState + 1)
+    setNum((prevState) => prevState + 1)
+    setNum((prevState) => prevState + 1)
+    setNum((prevState) => prevState + 1)
   }
-  const handleInput = (e) => {
-    // setFormObj({
-    //   ...formObj,
-    //   [e.target.name] : e.target.value
-    // })
-    setFormObj((prevObj) => ({...prevObj, [e.target.name] : e.target.value}))
-  };
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="firstName" onInput={handleInput} />
-        <input type="text" name="lastName" onInput={handleInput} />
-        <input type="text" name="address" onInput={handleInput} />
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-      <div>
-        <li>{formObj.firstName}</li>
-      </div>
+      <h1>Number : {num} </h1>
+      <button onClick={handleClick}>Click</button>
     </>
   );
 };
