2c00254b64b47ecdb181e718b712309691c5c358
tanvirtbf <tanver018765@gmail.com>
2024-05-13 11:01:35 +0600

    start with cleaning up function

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,56 +1,11 @@
 import "./App.css";
+import Destroy from "./components/destroy";
 
 const App = () => {
+
   return (
     <>
-      <div className="main">
-        <h1>My name is Tanvir</h1>
-        <h4>I am a Developer</h4>
-        <p>Here is my skill : </p>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Javascript</li>
-          <li>React</li>
-          <li>C++</li>
-        </ul>
-      </div>
-      <div className="main">
-        <h1>My name is Sabbir</h1>
-        <h4>I am a Developer</h4>
-        <p>Here is my skill : </p>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Javascript</li>
-          <li>React</li>
-          <li>C++</li>
-        </ul>
-      </div>
-      <div className="main">
-        <h1>My name is Rifat</h1>
-        <h4>I am a Developer</h4>
-        <p>Here is my skill : </p>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Javascript</li>
-          <li>React</li>
-          <li>C++</li>
-        </ul>
-      </div>
-      <div className="main">
-        <h1>My name is Riduan</h1>
-        <h4>I am a Developer</h4>
-        <p>Here is my skill : </p>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Javascript</li>
-          <li>React</li>
-          <li>C++</li>
-        </ul>
-      </div>
+      <Destroy />
     </>
   );
 };
