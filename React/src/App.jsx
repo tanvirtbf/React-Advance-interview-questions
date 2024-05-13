4d3b9278e199ef485f774ca1d2e44fe07f1f873b
tanvirtbf <tanver018765@gmail.com>
2024-05-13 15:38:24 +0600

    custom modal using basic code by me

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,9 +1,25 @@
-import InputRef from './components/InputRef'
+import { useState } from 'react'
+import './App.css'
+ const Modal = ({setIsOpenModal,isOpenModal}) => {
+  return (
+    <div className="modal">
+      <h1>Name : Tanvir</h1>
+      <p>A Frontend Developer</p>
+      <p>Very Dedicated..</p>
+      <button onClick={()=> setIsOpenModal(!isOpenModal)}>Close</button>
+    </div>
+  )
+}
 
 const App = () => {
+  const [isOpenModal,setIsOpenModal] = useState(false)
+  const handleOpenModal = ()=> {
+    setIsOpenModal(!isOpenModal)
+  }
   return (
     <div>
-      <InputRef />
+      {isOpenModal ? <Modal setIsOpenModal={setIsOpenModal} isOpenModal={isOpenModal}/> : null}
+      {isOpenModal ? null :<button onClick={handleOpenModal}>{isOpenModal? 'Close' : 'Open'} Modal</button>}
     </div>
   )
 }
