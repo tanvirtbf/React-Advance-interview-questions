8337c81ac70573bde283ac6331190010372aafd9
tanvirtbf <tanver018765@gmail.com>
2024-05-13 15:48:28 +0600

    let's create Modal Component using createPortal and useRef

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,28 +1,33 @@
-import { useState } from 'react'
-import './App.css'
- const Modal = ({setIsOpenModal,isOpenModal}) => {
+import { useState } from "react";
+import "./App.css";
+const Modal = ({ setIsOpenModal, isOpenModal }) => {
   return (
     <div className="modal">
       <h1>Name : Tanvir</h1>
       <p>A Frontend Developer</p>
       <p>Very Dedicated..</p>
-      <button onClick={()=> setIsOpenModal(!isOpenModal)}>Close</button>
+      <button onClick={() => setIsOpenModal(!isOpenModal)}>Close</button>
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
-  const [isOpenModal,setIsOpenModal] = useState(false)
-  const handleOpenModal = ()=> {
-    setIsOpenModal(!isOpenModal)
-  }
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const handleOpenModal = () => {
+    setIsOpenModal(!isOpenModal);
+  };
   return (
     <div>
-      {isOpenModal ? <Modal setIsOpenModal={setIsOpenModal} isOpenModal={isOpenModal}/> : null}
-      {isOpenModal ? null :<button onClick={handleOpenModal}>{isOpenModal? 'Close' : 'Open'} Modal</button>}
+      {isOpenModal ? (
+        <Modal setIsOpenModal={setIsOpenModal} isOpenModal={isOpenModal} />
+      ) : null}
+      {isOpenModal ? null : (
+        <button onClick={handleOpenModal}>
+          {isOpenModal ? "Close" : "Open"} Modal
+        </button>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
