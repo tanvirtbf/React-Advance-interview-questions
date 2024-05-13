a634488fc991a5495b2c9488b02a89fbf1252b19
tanvirtbf <tanver018765@gmail.com>
2024-05-13 14:12:57 +0600

    start with useRef and persist data across re -renders and controlled uncontrolled component

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,27 +1,12 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import React from 'react'
+
 const App = () => {
-  const [hour,setHour] = useState(11)
-  const [minute,setMinute] = useState(59)
-  const [second,setSecond] = useState(55)
-  
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setSecond(second+1)
-      if(second === 59){
-        setMinute(minute+1)
-        setSecond(0)
-      }
-      if(minute ===59){
-        setHour(hour+1)
-        setMinute(0)
-      }
-    },1000);
-    return ()=>{
-      console.log('clear')
-      clearInterval(interval)
-    }
-  }, [second,minute,hour]);
-  return <>{`${hour} : ${minute} : ${second}`}</>;
-};
-export default App;
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default App
+
