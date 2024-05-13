50cca84da03bbc3785f16a41a3d9ab1c9ed3bea6
tanvirtbf <tanver018765@gmail.com>
2024-05-13 14:11:42 +0600

    create custom watch using react and js logic without predefined date method

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,20 +1,27 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-
 const App = () => {
-  const [count, setCount] = useState(0);
-
+  const [hour,setHour] = useState(11)
+  const [minute,setMinute] = useState(59)
+  const [second,setSecond] = useState(55)
+  
   useEffect(() => {
     const interval = setInterval(() => {
-      setCount(prevState => prevState+1);
+      setSecond(second+1)
+      if(second === 59){
+        setMinute(minute+1)
+        setSecond(0)
+      }
+      if(minute ===59){
+        setHour(hour+1)
+        setMinute(0)
+      }
     },1000);
-
     return ()=>{
+      console.log('clear')
       clearInterval(interval)
     }
-  }, []);
-
-  return <>{count}</>;
+  }, [second,minute,hour]);
+  return <>{`${hour} : ${minute} : ${second}`}</>;
 };
-
 export default App;
