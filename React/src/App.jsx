bed45ad8d555cd9f530fafce43057f4881e609c1
tanvirtbf <tanver018765@gmail.com>
2024-05-14 09:46:22 +0600

    start again coding for previous project with alone

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,10 +1,9 @@
 import './App.css'
-import Modal from './components/Modal'
 
 const App = () => {
   return (
     <div>
-      <Modal />
+      
     </div>
   )
 }
---
+++ React/src/components/DuplicateModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DuplicateModal = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DuplicateModal
