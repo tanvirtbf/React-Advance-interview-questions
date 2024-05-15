5317ff7a1835f1f247560ee2f78d80bf5b9501d6
tanvirtbf <tanver018765@gmail.com>
2024-05-15 17:03:30 +0600

    alternative memo is use separate component

--- React/src/components/UseCallBack.jsx
+++ React/src/components/UseCallBack.jsx
@@ -1,35 +1,37 @@
-import { useCallback, useMemo, useState } from 'react'
-import ExpensiveComp from './ExpensiveComp'
+import { useState } from "react";
+import ExpensiveComp from "./ExpensiveComp";
 
 const samplePosts = [
-  {id:1,title:'Hello, Iam Tanvir'},
-  {id:2,title:'Hello, I am Sabbir'},
-  {id:3,title:'Hello, I am Rifat'},
-  {id:4,title:'Hello, I am Riduan'},
-  {id:5,title:'Hello, I am Mustak'},
-]
+  { id: 1, title: "Hello, Iam Tanvir" },
+  { id: 2, title: "Hello, I am Sabbir" },
+  { id: 3, title: "Hello, I am Rifat" },
+  { id: 4, title: "Hello, I am Riduan" },
+  { id: 5, title: "Hello, I am Mustak" },
+];
+
+const Posts = () => {
+  const [posts, setPosts] = useState(samplePosts);
+  const addPost = () => {
+    setPosts((prevState) => [
+      ...prevState,
+      { id: Date.now(), title: "A new Title is here" },
+    ]);
+  };
+  return <ExpensiveComp posts={posts} addPost={addPost} />;
+};
+
+const Count = () => {
+  const [count, setCount] = useState(0);
+  return <button onClick={() => setCount(count + 1)}>{count}</button>;
+};
 
 const UseCallBack = () => {
-  const [posts,setPosts] = useState(samplePosts)
-  const [count,setCount] = useState(0)
-
-  const addPost = useCallback(()=>{
-    setPosts((prevState)=>[
-      ...prevState,
-      {id:Date.now(),title:"A new Title is here"}
-    ])
-  },[])
-
-  const ExpensivePostMemo = useMemo(()=>{
-    return <ExpensiveComp posts={posts} addPost={addPost}/>
-  },[posts,addPost])
-
   return (
     <div>
-      {ExpensivePostMemo}
-      <button onClick={()=> setCount(count+1)}>{count}</button>
+      <Posts />
+      <Count />
     </div>
-  )
-}
+  );
+};
 
-export default UseCallBack
+export default UseCallBack;
