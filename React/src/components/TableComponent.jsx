262671c4323346050b35af83e06eb5e06d915daf
tanvirtbf <tanver018765@gmail.com>
2024-05-18 10:06:34 +0600

    table loading without reusable table data

--- React/src/components/TableComponent.jsx
+++ React/src/components/TableComponent.jsx
@@ -6,11 +6,24 @@ const TableComponent = ({data}) => {
       <table>
         <thead>
           <tr>
-            <th>sdas</th>
-            <th>sdas</th>
-            <th>sdas</th>
+            {Object.keys(data[0]).map((heading)=> <th key={heading}>{heading}</th>)}
           </tr>
         </thead>
+        <tbody>
+          {
+            data.map((row)=> {
+              return (
+                <tr key={row.id}>
+                  <td>{row.id}</td>
+                  <td>{row.name}</td>
+                  <td>{row.price}</td>
+                  <td>{row.category}</td>
+                  <td>{row.stock}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
       </table>
     </div>
   )
