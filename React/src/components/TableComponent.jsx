0d1ebe8a6c46df3170ef7303f3d15ff80c4f6108
tanvirtbf <tanver018765@gmail.com>
2024-05-18 11:37:10 +0600

    complete reusable table component

--- React/src/components/TableComponent.jsx
+++ React/src/components/TableComponent.jsx
@@ -1,35 +1,42 @@
 import React, { useMemo, useState } from "react";
 
-const TableComponent = ({data}) => {
-  const [search,setSearch] = useState("")
+const TableComponent = ({ data }) => {
+  const [search, setSearch] = useState("");
 
-  const filteredData = data.filter((rowObj)=> {
+  const filteredData = data.filter((rowObj) => {
     return Object.values(rowObj).some(
-      value => value && value.toString().toLowerCase().includes(search.toLowerCase())
-    )
-  })
-  
-  const headings = useMemo(()=> {
-    return Object.keys(data[0]).map(heading => <th key={heading}>{heading}</th>)
-  },[data])
+      (value) =>
+        value && value.toString().toLowerCase().includes(search.toLowerCase())
+    );
+  });
 
-  const tableData = filteredData.map((row)=> <tr key={row.id}>{Object.values(row).map(item=> <td key={item}>{item}</td>)}</tr>)
+  const headings = useMemo(() => {
+    return Object.keys(data[0]).map((heading) => (
+      <th key={heading}>{heading}</th>
+    ));
+  }, [data]);
 
-
-
+  const tableData = filteredData.map((row) => (
+    <tr key={row.id}>
+      {Object.values(row).map((item) => (
+        <td key={item}>{item}</td>
+      ))}
+    </tr>
+  ));
 
   return (
     <div>
-      <input type="text" placeholder="search table component" onChange={(e)=> setSearch(e.target.value)} value={search} />
+      <input
+        type="text"
+        placeholder="search table component"
+        onChange={(e) => setSearch(e.target.value)}
+        value={search}
+      />
       <table>
         <thead>
-          <tr>
-            {headings}
-          </tr>
+          <tr>{headings}</tr>
         </thead>
-        <tbody>
-          {tableData}
-        </tbody>
+        <tbody>{tableData}</tbody>
       </table>
     </div>
   );
