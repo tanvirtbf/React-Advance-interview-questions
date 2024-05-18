aa11bc2620fa43dd44c39c16dd119c8c8c59bfca
tanvirtbf <tanver018765@gmail.com>
2024-05-18 11:36:00 +0600

    optimized table heading using useMemo hook

--- React/src/components/TableComponent.jsx
+++ React/src/components/TableComponent.jsx
@@ -1,14 +1,21 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 
 const TableComponent = ({data}) => {
   const [search,setSearch] = useState("")
-
 
   const filteredData = data.filter((rowObj)=> {
     return Object.values(rowObj).some(
       value => value && value.toString().toLowerCase().includes(search.toLowerCase())
     )
   })
+  
+  const headings = useMemo(()=> {
+    return Object.keys(data[0]).map(heading => <th key={heading}>{heading}</th>)
+  },[data])
+
+  const tableData = filteredData.map((row)=> <tr key={row.id}>{Object.values(row).map(item=> <td key={item}>{item}</td>)}</tr>)
+
+
 
 
   return (
@@ -17,11 +24,11 @@ const TableComponent = ({data}) => {
       <table>
         <thead>
           <tr>
-            {Object.keys(data[0]).map(heading => <th key={heading}>{heading}</th>)}
+            {headings}
           </tr>
         </thead>
         <tbody>
-          {filteredData.map((row)=> <tr key={row.id}>{Object.values(row).map(item=> <td key={item}>{item}</td>)}</tr>)}
+          {tableData}
         </tbody>
       </table>
     </div>
