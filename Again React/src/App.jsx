d421294aa21c3a5c3df0d9ad374ee1edcbffea59
tanvirtbf <tanver018765@gmail.com>
2024-07-14 11:18:14 +0600

    deeply know the synthetic event with react and js

--- Again React/src/App.jsx
+++ Again React/src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
 
 function App() {
-  const handleInput = (text) => {
+  const handleInput = (e,text) => {
+    console.log(e)
     console.log(text)
     console.log("Handle Input Called")
   }
   return (
     <>
       <h2>Event Handling</h2>
-      <input type="text" onInput={()=>handleInput("hello")} />
+      <input type="text" onInput={(e)=>handleInput(e,"hello")} />
     </>
   )
 }
