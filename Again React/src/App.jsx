41872e086aee1269fd8c835ff8b8354791c02c69
tanvirtbf <tanver018765@gmail.com>
2024-07-14 10:13:16 +0600

    list render using map

--- Again React/src/App.jsx
+++ Again React/src/App.jsx
@@ -2,9 +2,15 @@ import "./App.css";
 import { Fragment } from "react";
 
 function App() {
+  const items  = [
+    "tanvir",
+    "sadia",
+    "maymuna"
+  ]
   return (
     <Fragment>
       <h1>Hello World!</h1>
+      <div>{items.map((item)=> <h1 key={item}>{item}</h1>)}</div>
     </Fragment>
   );
 }
