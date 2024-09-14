dad719d2df0232c00897df355ee3453a863f98c1
tanvirtbf <tanver018765@gmail.com>
2024-09-14 10:59:01 +0600

    project done by another approach

--- Basic React/index.js
+++ Basic React/index.js
@@ -1,32 +1,77 @@
-let num = 100;
-
-let leftApples = document.querySelector('.busket-1 > h1 > span').innerHTML
-let rightApples = document.querySelector('.busket-2 > h1 > span').innerHTML
-
-let leftNumber = Number(leftApples)
-let rightNumber = Number(rightApples)
+const firstBusket = document.querySelector('.busket-1 > h1 > span')
+const secondBusket = document.querySelector('.busket-2 > h1 > span')
 
 const leftBtn = document.querySelector('.left-arrow')
 const rightBtn = document.querySelector('.right-arrow')
 
+let totalBusket = 10
+
+let rightBusketAmount = 0
+let leftBusketAmount = totalBusket - rightBusketAmount
+
+firstBusket.innerText = leftBusketAmount
+secondBusket.innerText = rightBusketAmount
+
 leftBtn.addEventListener('click',()=>{
-  if(leftNumber<100){
-    leftNumber += 1
-    rightNumber -= 1
+  if(rightBusketAmount>0){
+    leftBusketAmount++
+    rightBusketAmount--
+    firstBusket.innerText = leftBusketAmount
+    secondBusket.innerText = rightBusketAmount
   }
-  document.querySelector('.busket-1 > h1 > span').innerHTML = leftNumber.toString()
-  document.querySelector('.busket-2 > h1 > span').innerHTML = rightNumber.toString()
 })
 
 rightBtn.addEventListener('click',()=>{
-  if(rightNumber<100){
-    rightNumber += 1
-    leftNumber -= 1
+  if(leftBusketAmount>0){
+    rightBusketAmount++
+    leftBusketAmount--
+    firstBusket.innerText = leftBusketAmount
+    secondBusket.innerText = rightBusketAmount
   }
-  document.querySelector('.busket-1 > h1 > span').innerHTML = leftNumber.toString()
-  document.querySelector('.busket-2 > h1 > span').innerHTML = rightNumber.toString()
-  
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num = 100;
+
+// let leftApples = document.querySelector('.busket-1 > h1 > span').innerHTML
+// let rightApples = document.querySelector('.busket-2 > h1 > span').innerHTML
+
+// let leftNumber = Number(leftApples)
+// let rightNumber = Number(rightApples)
+
+// const leftBtn = document.querySelector('.left-arrow')
+// const rightBtn = document.querySelector('.right-arrow')
+
+// leftBtn.addEventListener('click',()=>{
+//   if(leftNumber<100){
+//     leftNumber += 1
+//     rightNumber -= 1
+//   }
+//   document.querySelector('.busket-1 > h1 > span').innerHTML = leftNumber.toString()
+//   document.querySelector('.busket-2 > h1 > span').innerHTML = rightNumber.toString()
+// })
+
+// rightBtn.addEventListener('click',()=>{
+//   if(rightNumber<100){
+//     rightNumber += 1
+//     leftNumber -= 1
+//   }
+//   document.querySelector('.busket-1 > h1 > span').innerHTML = leftNumber.toString()
+//   document.querySelector('.busket-2 > h1 > span').innerHTML = rightNumber.toString()
+// })
 
 
 
