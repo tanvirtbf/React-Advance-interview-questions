b8aa39de144a27d3c15c07726c4e8361ce9e9d69
tanvirtbf <tanver018765@gmail.com>
2024-09-14 12:19:02 +0600

    project ui complete by custom reactElement

--- Basic React/index.js
+++ Basic React/index.js
@@ -1,81 +1,33 @@
-const firstBusket = document.querySelector('.busket-1 > h1 > span')
-const secondBusket = document.querySelector('.busket-2 > h1 > span')
+const leftArrow = React.createElement('img',{className: 'left-arrow', src:'./images/left-arrow.png'})
+const rightArrow = React.createElement('img',{className: 'right-arrow', src:'./images/right-arrow.png'})
 
-const leftBtn = document.querySelector('.left-arrow')
-const rightBtn = document.querySelector('.right-arrow')
-
-let totalBusket = 10
-
-let rightBusketAmount = 0
-let leftBusketAmount = totalBusket - rightBusketAmount
-
-firstBusket.innerText = leftBusketAmount
-secondBusket.innerText = rightBusketAmount
-
-leftBtn.addEventListener('click',()=>{
-  if(rightBusketAmount>0){
-    leftBusketAmount++
-    rightBusketAmount--
-    firstBusket.innerText = leftBusketAmount
-    secondBusket.innerText = rightBusketAmount
-  }
-})
-
-rightBtn.addEventListener('click',()=>{
-  if(leftBusketAmount>0){
-    rightBusketAmount++
-    leftBusketAmount--
-    firstBusket.innerText = leftBusketAmount
-    secondBusket.innerText = rightBusketAmount
-  }
-})
+const btn1 = React.createElement('button',{className: 'leftBtn'},leftArrow)
+const btn2 = React.createElement('button',{className: 'rightBtn'},rightArrow)
 
 
 
-// 3 Primary Reasons to learn React -
-// 1. Declarative Programming
-// 2. Component Architecture
-// 3. Single Page Application
+const heading1 = React.createElement('h1',{className:'busket1'},'10')
+const heading2 = React.createElement('h1',{className:'busket2'},'0')
+
+const div1 = React.createElement('div',{className:'div1',id:'div1'},heading1)
+const div2 = React.createElement('div',{className:'div2',id:'div2'},heading2)
+
+
+
+const section = React.createElement('section',{className:'section'},div1,btn1,btn2,div2)
+
+
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+
+
+root.render(section)
 
 
 
 
 
-
-
-
-
-
-
-
-// let num = 100;
-
-// let leftApples = document.querySelector('.busket-1 > h1 > span').innerHTML
-// let rightApples = document.querySelector('.busket-2 > h1 > span').innerHTML
-
-// let leftNumber = Number(leftApples)
-// let rightNumber = Number(rightApples)
-
-// const leftBtn = document.querySelector('.left-arrow')
-// const rightBtn = document.querySelector('.right-arrow')
-
-// leftBtn.addEventListener('click',()=>{
-//   if(leftNumber<100){
-//     leftNumber += 1
-//     rightNumber -= 1
-//   }
-//   document.querySelector('.busket-1 > h1 > span').innerHTML = leftNumber.toString()
-//   document.querySelector('.busket-2 > h1 > span').innerHTML = rightNumber.toString()
-// })
-
-// rightBtn.addEventListener('click',()=>{
-//   if(rightNumber<100){
-//     rightNumber += 1
-//     leftNumber -= 1
-//   }
-//   document.querySelector('.busket-1 > h1 > span').innerHTML = leftNumber.toString()
-//   document.querySelector('.busket-2 > h1 > span').innerHTML = rightNumber.toString()
-// })
 
 
 
