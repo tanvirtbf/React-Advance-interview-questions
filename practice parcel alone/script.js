0eccf67877235fb7f89b14ff553f13db39572a28
tanvirtbf <tanver018765@gmail.com>
2024-09-21 09:13:53 +0600

    start

--- practice parcel alone/script.js
+++ practice parcel alone/script.js
@@ -1,20 +1,8 @@
-import React from 'react'
-import {createRoot} from 'react-dom/client'
+const { createRoot } = require("react-dom/client");
 
-const card = React.createElement('div',{className:'card', style:{display:'flex', flexWrap:'wrap',gap:'20px'}},
-  [
-    React.createElement('img',{key:1, className:'image', src:'https://beautybooth.com.bd/_next/image?url=https%3A%2F%2Fadmin.beautybooth.com.bd%2F%2Fuploads%2Fall%2F2FK8ylB5lJrDP9PnIGzensEaeaDcvT7jAD0ZGLxH.jpg&w=1920&q=75',alt:'image',style:{width:'200px', border:'1px solid'}}),
-    React.createElement('img',{key:2, className:'image', src:'https://beautybooth.com.bd/_next/image?url=https%3A%2F%2Fadmin.beautybooth.com.bd%2F%2Fuploads%2Fall%2F2FK8ylB5lJrDP9PnIGzensEaeaDcvT7jAD0ZGLxH.jpg&w=1920&q=75',alt:'image',style:{width:'200px', border:'1px solid'}}),
-    React.createElement('img',{key:3, className:'image', src:'https://beautybooth.com.bd/_next/image?url=https%3A%2F%2Fadmin.beautybooth.com.bd%2F%2Fuploads%2Fall%2F2FK8ylB5lJrDP9PnIGzensEaeaDcvT7jAD0ZGLxH.jpg&w=1920&q=75',alt:'image',style:{width:'200px', border:'1px solid'}}),
-    React.createElement('img',{key:4, className:'image', src:'https://beautybooth.com.bd/_next/image?url=https%3A%2F%2Fadmin.beautybooth.com.bd%2F%2Fuploads%2Fall%2F2FK8ylB5lJrDP9PnIGzensEaeaDcvT7jAD0ZGLxH.jpg&w=1920&q=75',alt:'image',style:{width:'200px', border:'1px solid'}}),
-    React.createElement('img',{key:5, className:'image', src:'https://beautybooth.com.bd/_next/image?url=https%3A%2F%2Fadmin.beautybooth.com.bd%2F%2Fuploads%2Fall%2F2FK8ylB5lJrDP9PnIGzensEaeaDcvT7jAD0ZGLxH.jpg&w=1920&q=75',alt:'image',style:{width:'200px', border:'1px solid'}}),
-    React.createElement('img',{key:6, className:'image', src:'https://beautybooth.com.bd/_next/image?url=https%3A%2F%2Fadmin.beautybooth.com.bd%2F%2Fuploads%2Fall%2F2FK8ylB5lJrDP9PnIGzensEaeaDcvT7jAD0ZGLxH.jpg&w=1920&q=75',alt:'image',style:{width:'200px', border:'1px solid'}}),
-    React.createElement('img',{key:7, className:'image', src:'https://beautybooth.com.bd/_next/image?url=https%3A%2F%2Fadmin.beautybooth.com.bd%2F%2Fuploads%2Fall%2F2FK8ylB5lJrDP9PnIGzensEaeaDcvT7jAD0ZGLxH.jpg&w=1920&q=75',alt:'image',style:{width:'200px', border:'1px solid'}}),
-    React.createElement('img',{key:8, className:'image', src:'https://beautybooth.com.bd/_next/image?url=https%3A%2F%2Fadmin.beautybooth.com.bd%2F%2Fuploads%2Fall%2F2FK8ylB5lJrDP9PnIGzensEaeaDcvT7jAD0ZGLxH.jpg&w=1920&q=75',alt:'image',style:{width:'200px', border:'1px solid'}}),
-    React.createElement('img',{key:9, className:'image', src:'https://beautybooth.com.bd/_next/image?url=https%3A%2F%2Fadmin.beautybooth.com.bd%2F%2Fuploads%2Fall%2F2FK8ylB5lJrDP9PnIGzensEaeaDcvT7jAD0ZGLxH.jpg&w=1920&q=75',alt:'image',style:{width:'200px', border:'1px solid'}})
-  ]
-)
+const h1 = <h1>Hello World</h1>
 
 const root = createRoot(document.getElementById('root'))
+root.render(h1)
 
-root.render(card)
+console.log('Hello World!!')
